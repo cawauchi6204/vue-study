4ccf5cadb2e3ed5b11f8b183d52ab28c17ea3c89
河内達也 <cawauchi6204@gmail.com>
2024-06-15 19:10:44 +0900

    update: propsにリアクティブ変数を渡す実装

--- directive-model/src/App.vue
+++ directive-model/src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import OneInfo from "./components/OneInfo.vue"
+
+const propsTitle = ref("発生した乱数");
+const rand = Math.round(Math.random() * 100);
+const propsContent = ref(rand);
 </script>
 
 <template>
   <h1>Props基礎</h1>
   <section>
     <h2>属性に直接記述</h2>
-    <OneInfo title="Propsの利用" content="子コンポーネントにデータを渡すにはPropsを利用する" />
+    <!-- リアクティブ変数をpropsとして渡すときはv-bindしないといけない -->
+    <OneInfo :title="propsTitle" :content="propsContent" />
   </section>
 </template>
 
