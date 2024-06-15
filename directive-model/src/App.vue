5d38d4cd52809a986347d7b4414ac48d9c9e01f5
河内達也 <cawauchi6204@gmail.com>
2024-06-15 16:08:30 +0900

    update: ウォッチャーを使用しないでリアクティブ変数の値の変化に応じて特定の処理を実行

--- directive-model/src/App.vue
+++ directive-model/src/App.vue
@@ -1,39 +1,31 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 
-const cocktailDataListInit = [
+// メモ computedは非同期処理には向いていない
+
+type Cocktail = {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const cocktailDataListInit: Cocktail[] = [
   { id: 2345, name: "ホワイト・レディ", price: 1200 },
   { id: 4412, name: "ブルーハワイ", price: 1500 },
   { id: 6789, name: "ニューヨーク", price: 1700 },
   { id: 3439, name: "マティーニ", price: 1900 },
 ]
-const cocktailDataList = ref(cocktailDataListInit)
-const cocktail1500 = computed(
-  () => {
-    const newList = cocktailDataList.value.filter(
-      (cocktailItem) => {
-        return cocktailItem.price == 1500
-      }
-    )
-    return newList
-  }
-)
+const selectedCocktail = ref(cocktailDataListInit[0])
+setInterval(() => {
+  const rand = Math.floor(Math.random() * cocktailDataListInit.length)
+  const randomCocktail = cocktailDataListInit[rand]
+  selectedCocktail.value = randomCocktail
+}, 1000)
 </script>
 
 <template>
-  <section>すべてのカクテルリスト
-    <ul>
-      <li v-for="cocktailData in cocktailDataList" v-bind:key="cocktailData.id">
-        {{ cocktailData.id }}: {{ cocktailData.name }}: {{ cocktailData.price }}円
-      </li>
-    </ul>
-  </section>
   <section>
-    値段が1500円のカクテルリスト
-    <ul>
-      <li v-for="cocktail in cocktail1500" v-bind:key="cocktail.id">
-        {{ cocktail.id }}: {{ cocktail.name }}: {{ cocktail.price }}円
-      </li>
-    </ul>
+    <h1>ランダムカクテル</h1>
+    <p>今日のおすすめは「{{ selectedCocktail.name }} {{ selectedCocktail.price }}円」です。</p>
   </section>
 </template>
