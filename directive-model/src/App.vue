091c47caa5af1cd056693a02d6e70976190b7479
河内達也 <cawauchi6204@gmail.com>
2024-06-15 15:37:07 +0900

    update: v-for

--- directive-model/src/App.vue
+++ directive-model/src/App.vue
@@ -1,13 +1,39 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
-const inputNameModel = ref("双方向")
+const cocktailDataListInit = [
+  { id: 2345, name: "ホワイト・レディ", price: 1200 },
+  { id: 4412, name: "ブルーハワイ", price: 1500 },
+  { id: 6789, name: "ニューヨーク", price: 1700 },
+  { id: 3439, name: "マティーニ", price: 1900 },
+]
+const cocktailDataList = ref(cocktailDataListInit)
+const cocktail1500 = computed(
+  () => {
+    const newList = cocktailDataList.value.filter(
+      (cocktailItem) => {
+        return cocktailItem.price == 1500
+      }
+    )
+    return newList
+  }
+)
 </script>
 
 <template>
+  <section>すべてのカクテルリスト
+    <ul>
+      <li v-for="cocktailData in cocktailDataList" v-bind:key="cocktailData.id">
+        {{ cocktailData.id }}: {{ cocktailData.name }}: {{ cocktailData.price }}円
+      </li>
+    </ul>
+  </section>
   <section>
-    <!-- v-model = v-bindとv-on:inputの組み合わせ -->
-    <input type="text" v-model="inputNameModel">
-    <p>{{ inputNameModel }}</p>
+    値段が1500円のカクテルリスト
+    <ul>
+      <li v-for="cocktail in cocktail1500" v-bind:key="cocktail.id">
+        {{ cocktail.id }}: {{ cocktail.name }}: {{ cocktail.price }}円
+      </li>
+    </ul>
   </section>
 </template>
