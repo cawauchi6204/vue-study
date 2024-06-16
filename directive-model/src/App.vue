adffce5be78b6b811a5da3ad2611d601f2f78769
河内達也 <cawauchi6204@gmail.com>
2024-06-16 11:48:51 +0900

    update: slotの実装

--- directive-model/src/App.vue
+++ directive-model/src/App.vue
@@ -1,36 +1,17 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import OneMember from "./components/OneMember.vue"
+import { ref } from "vue";
+import OneSection from "./components/OneSection.vue";
 
-const memberListInit = new Map<number, Member>();
-memberListInit.set(33356, { id: 33356, name: "山田太郎", email: "bow@example.com", points: 35, note: "初回入会特典あり" });
-memberListInit.set(47783, { id: 47783, name: "鈴木花子", email: "mue@example.com", points: 53 })
-const memberList = ref(memberListInit)
-
-const totalPoints = computed(
-  () => {
-    let total = 0;
-    for (const member of memberList.value.values()) {
-      total += member.points;
-    }
-    return total
-  }
-)
-
-interface Member {
-  id: number
-  name: string
-  email: string
-  points: number
-  note?: string
-}
+const taro = ref("田中太郎")
 </script>
 
 <template>
-  <h1>会員リスト</h1>
-  <p>全会員の保有ポイントの合計: {{ totalPoints }}</p>
-  <OneMember v-for="[id, member] in memberList" :key="id" :id="id" :name="member.name" :email="member.email"
-    :note="member.note" v-model:points="member.points" />
+  <section>
+    <h2>slotの利用</h2>
+    <OneSection :name="taro">
+      <p>連絡がつきません</p>
+    </OneSection>
+  </section>
 </template>
 
 <style>
--- directive-model/src/components/OneSection.vue
+++ directive-model/src/components/OneSection.vue
@@ -1,23 +1,16 @@
 <script setup lang="ts">
 interface Props {
-  rand: number
-}
-interface Emits {
-  (event: "createNewRand"): void
+  name: string
 }
 
 defineProps<Props>()
-const emit = defineEmits<Emits>()
-
-const onNewRandButtonClick = () => {
-  emit("createNewRand")
-}
 </script>
 
 <template>
   <section class="box">
-    <p>子コンポーネントで乱数を表示: {{ rand }}</p>
-    <button @click="onNewRandButtonClick">新たな乱数を発生</button>
+    <h1>{{ name }}さんの状況</h1>
+    <!-- reactでいうchildrenのようなもの -->
+    <slot />
   </section>
 </template>
 
