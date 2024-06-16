fb70b748a93f68b3f247c56960b2e77ad012edca
河内達也 <cawauchi6204@gmail.com>
2024-06-16 10:54:13 +0900

    update: 子から親の値を変更させるemitの実装

--- directive-model/src/App.vue
+++ directive-model/src/App.vue
@@ -1,36 +1,19 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import OneMember from "./components/OneMember.vue"
+import { ref } from 'vue';
+import OneSection from './components/OneSection.vue';
 
-const memberListInit = new Map<number, Member>();
-memberListInit.set(33356, { id: 33356, name: "山田太郎", email: "bow@example.com", points: 35, note: "初回入会特典あり" });
-memberListInit.set(47783, { id: 47783, name: "鈴木花子", email: "mue@example.com", points: 53 })
-const memberList = ref(memberListInit)
-
-const totalPoints = computed(
-  () => {
-    let total = 0;
-    for (const member of memberList.value.values()) {
-      total += member.points;
-    }
-    return total
-  }
-)
-
-interface Member {
-  id: number
-  name: string
-  email: string
-  points: number
-  note?: string
+const randInit = Math.round(Math.random() * 10)
+const rand = ref(randInit)
+const onCreateNewRand = () => {
+  rand.value = Math.round(Math.random() * 10)
 }
 </script>
 
 <template>
-  <h1>会員リスト</h1>
-  <p>全会員の保有ポイントの合計: {{ totalPoints }}</p>
-  <OneMember v-for="[id, member] in memberList" :key="id" :id="id" :name="member.name" :email="member.email"
-    :points="member.points" :note="member.note" />
+  <section>
+    <p>親コンポーネントで乱数を表示: {{ rand }}</p>
+    <OneSection :rand="rand" :onCreateNewRand="onCreateNewRand" />
+  </section>
 </template>
 
 <style>
--- directive-model/src/components/OneSection.vue
+++ directive-model/src/components/OneSection.vue
@@ -1,11 +1,27 @@
+<script setup lang="ts">
+interface Props {
+  rand: number
+}
+interface Emits {
+  (event: "createNewRand"): void
+}
+
+defineProps<Props>()
+const emit = defineEmits<Emits>()
+
+const onNewRandButtonClick = () => {
+  emit("createNewRand")
+}
+</script>
+
 <template>
   <section class="box">
-    <h4>ひとつのコンポーネント</h4>
-    <p>コンポーネントとは、</p>
+    <p>子コンポーネントで乱数を表示: {{ rand }}</p>
+    <button @click="onNewRandButtonClick">新たな乱数を発生</button>
   </section>
 </template>
 
-<style>
+<style scoped>
 .box {
   border: green 1px dashed;
   margin: 10px;
