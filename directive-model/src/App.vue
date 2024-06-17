c1677a06e2bd1d56501bb98ff1609c163153efcd
河内達也 <cawauchi6204@gmail.com>
2024-06-17 09:38:53 +0900

    update: 動的コンポーネントとKeepAliveの利用

--- directive-model/src/App.vue
+++ directive-model/src/App.vue
@@ -1,31 +1,35 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import OneSection from "./components/OneSection.vue";
+import Input from "./components/Input.vue";
+import Radio from "./components/Radio.vue";
+import Select from "./components/Select.vue";
 
-const taro = ref("田中太郎")
+const currentComp = ref(Input);
+const currentCompName = ref("Input");
+
+const compList = [Input, Radio, Select]
+const compNameList: string[] = ["Input", "Radio", "Select"]
+
+let currentCompIndex = 0;
+const switchComp = () => {
+  currentCompIndex++;
+  if (currentCompIndex >= compList.length) {
+    currentCompIndex = 0;
+  }
+
+  currentComp.value = compList[currentCompIndex];
+  currentCompName.value = compNameList[currentCompIndex];
+}
 </script>
 
 <template>
-  <section>
-    <h2>slotの利用</h2>
-    <OneSection :name="taro">
-      <!-- 名前なしのslotはdefaultになる -->
-      <!-- defaultをちゃんと明記しないと表示されない -->
-      <template v-slot:default>
-        <!-- <template #default> -->
-        <!-- この書き方でもおけ -->
-        <p>問題発生</p>
-      </template>
-      <template v-slot:detail>
-        <!-- <template #detail> -->
-        <!-- この書き方でもおけ -->
-        <ul>
-          <li>ほげ</li>
-        </ul>
-      </template>
-    </OneSection>
-    <OneSection name="ziro" />
-  </section>
+  <p>コンポーネント名: {{ currentCompName }}</p>
+  <!-- KeepAliveがあるとそれまでの入力内容(リアクティブ変数)が保持される -->
+  <!-- 動的コンポーネントはv-bind:isを使用してコンポーネントを渡す -->
+  <KeepAlive>
+    <component v-bind:is="currentComp" />
+  </KeepAlive>
+  <button @click="switchComp">切り替え</button>
 </template>
 
 <style>
